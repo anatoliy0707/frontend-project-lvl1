c7178b163a3e56a6ae519ce640153c969a336fef
anatoliy0707 <anatoliy_sukhanov@mail.ru>
2021-01-24 23:08:03 +0500

    third step finished

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import read from '../src/cli.js';
+import greeting from '../src/cli.js';
 
 console.log("Welcome to the Brain Games!")
-console.log('Hello, ' + read() + '!');
+console.log('Hello, ' + greeting() + '!');
