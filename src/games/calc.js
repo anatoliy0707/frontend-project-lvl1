0a4aad69e0dcf467b4fbb06b7325258c7b5c8f8d
anatoliy0707 <anatoliy_sukhanov@mail.ru>
2021-02-03 21:09:48 +0500

    fix calc no eval

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,12 +4,25 @@ import gameLogic from "../index.js";
 const getRandomOperator = (coll = ["+", "-", "*"]) =>
   coll[Math.floor(Math.random() * coll.length)];
 
+const getExpression = (operandOne, operandTwo, operator) => {
+  switch (operator) {
+    case "+":
+      return operandOne + operandTwo;
+    case "-":
+      return operandOne - operandTwo;
+    case "*":
+      return operandOne * operandTwo;
+    default:
+      return false;
+  }
+};
+
 const calcGameData = () => {
   const num1 = getRandomInRange(1, 100);
   const num2 = getRandomInRange(1, 100);
   const operator = getRandomOperator();
   const question = `${num1} ${operator} ${num2}`;
-  const rightAnswer = String(eval(question));
+  const rightAnswer = String(getExpression(num1, num2, operator));
   const result = [question, rightAnswer];
   return result;
 };
